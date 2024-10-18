d2a8b5933619da9b858615386bf8baff7bf0555f
carltechzz <carlchichia@gmail.com>
2024-10-18 14:58:32 +0300

    ⭕️CARLTECH⭕️

--- commandes/carlr1.js
+++ commandes/carlr1.js
@@ -6,7 +6,7 @@ zokou({ nomCom: "speed", reaction: "🕳", nomFichier: __filename }, async (dest
     let z = '𝑯𝒆𝒍𝒍𝒐 𝒗𝒂𝒍𝒖𝒆𝒅 𝒖𝒔𝒆𝒓 𝒐𝒇 𝑪𝒚𝒃𝒆𝒓𝒊𝒐𝒏';
     let d = '𝑻𝒉𝒆 𝒂𝒃𝒐𝒗𝒆 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒊𝒔 𝒕𝒐 𝒊𝒏𝒇𝒐𝒓𝒎 𝒚𝒐𝒖 𝒕𝒉𝒂𝒕 𝑪𝒚𝒃𝒆𝒓𝒊𝒐𝒏 𝒊𝒔 𝑪𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒂𝒕 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅 𝒐𝒇 700.13/ms.';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/0e2976b99ebfc22a2753f.jpg';
+    var img = 'https://i.imgur.com/Jx5ksWK.jpeg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
